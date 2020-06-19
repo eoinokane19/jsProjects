a6bdf544f226876bc53935dea023c5476707ae10
eoinokane19 <eoin.okane5@gmail.com>
2020-06-19 19:22:18 +0100

    Updates to consulting case files

--- consultingCases/game.js
+++ consultingCases/game.js
@@ -11,26 +11,57 @@ function startGame() {
 function showTextNode(textNodeIndex) {
     const textNode = textNodes.find(textNode => textNode.id === textNodeIndex);
     textElement.innerText = textNode.text;
+    while(optionButtonsElement.firstChild) {
+        optionButtonsElement.removeChild(optionButtonsElement.firstChild);
+    }
+
+    textNode.options.forEach(option => {
+        if(showOption(option)) {
+            const button = document.createElement('button');
+            button.innerText = option.text;
+            button.classList.add('btn')
+            button.addEventListener('click', () => selectOption(option));
+            optionButtonsElement.appendChild(button);
+        }
+    })
+}
+
+function showOption(){
+    return true;
 }
 
 function selectOption(option) {
-
+    const nextTextNodeId = option.nextText;
+    state = Object.assign(state, option.setState)
+    showTextNode(nextTextNodeId)
 }
 
 const textNodes = [
     {
         id: 1,
-        text: 'Description of the case',
+        text: 'Case Study: BioPharma Expansion',
         options: [
             {
-                text: 'First Option',
-                setState: {},
+                text: 'Start',
+                setState: { },
                 nextText: 2
+            }
+        ]
+    },
+    {
+        id: 2,
+        text: 'Your client is BioPharma, a multinational healthcare company headquartered in the Netherlands.',
+        options: [
+            {
+                text: 'Inquire about revenue',
+                setState: {},
+                nextText: 3
             },
             {
-                text: 'Second Option',
-                nextTest: 2
-            }
+                text: 'Inquire about costs',
+                setState: {},
+                nextText: 3
+            }   
         ]
     }
 ]
